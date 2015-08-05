6cb8cee8a825044de972a3308db2573aca7fddad
Kevin Martensson <kevinmartensson@gmail.com>
2015-08-05 11:54:32 +0200

    Add sync method
    Fixes #3.

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var test = require('ava');
 var osLocale = require('./');
 
-test(function (t) {
+test('async', function (t) {
 	t.plan(2);
 
 	osLocale(function (err, locale) {
@@ -10,4 +10,12 @@ test(function (t) {
 		t.assert(locale.length > 1);
 		t.assert(locale.indexOf('_') !== -1);
 	});
+});
+
+test('sync', function (t) {
+	var locale = osLocale.sync();
+	console.log('Locale identifier:', locale);
+	t.assert(locale.length > 1);
+	t.assert(locale.indexOf('_') !== -1);
+	t.end();
 });
