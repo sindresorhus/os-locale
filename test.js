d4aa6635b73f851376fb38d5efce2d2f664f78dc
nexdrew <andrew.goode@nextraq.com>
2015-09-01 22:37:43 +0700

    Close #10 PR: Add option to disable spawning.

--- test.js
+++ test.js
@@ -1,22 +1,100 @@
 'use strict';
 var test = require('ava');
-var osLocale = require('./');
+var requireUncached = require('require-uncached');
+var envVars = ['LC_ALL', 'LANGUAGE', 'LANG', 'LC_MESSAGES'];
+var expectedFallback = 'en_US';
+
+function unsetEnvVars(cache) {
+	envVars.forEach(function (envVar) {
+		if (process.env[envVar]) {
+			cache[envVar] = process.env[envVar];
+			delete process.env[envVar];
+		}
+	});
+}
+
+function restoreEnvVars(cache) {
+	envVars.forEach(function (envVar) {
+		if (cache[envVar]) {
+			process.env[envVar] = cache[envVar];
+		}
+	});
+}
 
 test('async', function (t) {
 	t.plan(3);
 
-	osLocale(function (err, locale) {
+	requireUncached('./')(function (err, locale) {
 		console.log('Locale identifier:', locale);
-		t.assert(!err, err);
-		t.assert(locale.length > 1);
-		t.assert(locale.indexOf('_') !== -1);
+		t.error(err);
+		t.true(locale.length > 1);
+		t.not(locale.indexOf('_'), -1);
 	});
 });
 
 test('sync', function (t) {
-	var locale = osLocale.sync();
+	var locale = requireUncached('./').sync();
 	console.log('Locale identifier:', locale);
-	t.assert(locale.length > 1);
-	t.assert(locale.indexOf('_') !== -1);
+	t.true(locale.length > 1);
+	t.not(locale.indexOf('_'), -1);
+	t.end();
+});
+
+test.serial('async without spawn', function (t) {
+	t.plan(3);
+
+	var beforeTest = {};
+
+	// unset env vars and cache for restoration
+	unsetEnvVars(beforeTest);
+
+	// mock child_process.execFile
+	beforeTest.childProcessExecFile = require('child_process').execFile;
+	require('child_process').execFile = function () {
+		var args = Array.prototype.slice.call(arguments);
+		var execFileCb = args[args.length - 1];
+		if (typeof execFileCb === 'function') {
+			// passing Error here causes osLocale callback not to be called
+			execFileCb(null, 'spawn_NOTALLOWED');
+		}
+	};
+
+	// callback to restore env vars, undo mock, and reload os-locale for subsequent tests
+	var afterTest = function () {
+		restoreEnvVars(beforeTest);
+		require('child_process').execFile = beforeTest.childProcessExecFile;
+	};
+
+	// test async method
+	requireUncached('./')(function (err, locale) {
+		console.log('Locale identifier:', locale);
+		afterTest();
+		t.error(err);
+		t.is(locale, expectedFallback, 'Locale did not match expected fallback');
+		t.not(locale, 'spawn_NOTALLOWED', 'Attempted to spawn subprocess');
+	}, {spawn: false});
+});
+
+test.serial('sync without spawn', function (t) {
+	var beforeTest = {};
+
+	// unset env vars and cache for restoration
+	unsetEnvVars(beforeTest);
+
+	// mock exec-file-sync
+	beforeTest['exec-file-sync'] = require('exec-file-sync');
+	require.cache[require.resolve('exec-file-sync')].exports = function () {
+		t.false('Attempted to spawn subprocess');
+	};
+
+	// test sync method
+	var locale = requireUncached('./').sync({spawn: false});
+	console.log('Locale identifier:', locale);
+	t.is(locale, expectedFallback, 'Locale did not match expected fallback');
+
+	// restore env vars and undo mock
+	restoreEnvVars(beforeTest);
+	require.cache[require.resolve('exec-file-sync')].exports = beforeTest['exec-file-sync'];
+
 	t.end();
 });
