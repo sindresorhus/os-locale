1a575df6a47626bcc671944c3962dccbf1cb51a0
Pedro Miranda <p.miguel.tr@gmail.com>
2016-11-07 23:39:20 +0000

    using async functions in test.js, change test.serial to serial, remove execa.sync check in test

--- test.js
+++ test.js
@@ -22,20 +22,11 @@ function restoreEnvVars(cache) {
 	});
 }
 
-test.cb('async', t => {
-	t.plan(2);
-
-	requireUncached('./')()
-		.then(locale => {
-			console.log('Locale identifier:', locale);
-			t.true(locale.length > 1);
-			t.not(locale.indexOf('_'), -1);
-			t.end();
-		})
-		.catch(() => {
-			t.false('Promise rejected');
-			t.end();
-		});
+test('async', async t => {
+	const locale = await requireUncached('./')();
+	console.log('Locale identifier:', locale);
+	t.true(locale.length > 1);
+	t.not(locale.indexOf('_'), -1);
 });
 
 test('sync', t => {
@@ -45,9 +36,7 @@ test('sync', t => {
 	t.not(locale.indexOf('_'), -1);
 });
 
-test.cb.serial('async without spawn', t => {
-	t.plan(2);
-
+test('async without spawn', async t => {
 	const beforeTest = {};
 
 	// unset env vars and cache for restoration
@@ -64,33 +53,24 @@ test.cb.serial('async without spawn', t => {
 	};
 
 	// test async method
-	requireUncached('./')({spawn: false})
-		.then(locale => {
-			console.log('Locale identifier:', locale);
-			afterTest();
-			t.is(locale, expectedFallback, 'Locale did not match expected fallback');
-			t.not(locale, 'spawn_NOTALLOWED', 'Attempted to spawn subprocess');
-			t.end();
-		})
-		.catch(() => {
-			t.false('Promise rejected');
-			t.end();
-		});
+	const locale = await requireUncached('./')({spawn: false});
+	console.log('Locale identifier:', locale);
+	afterTest();
+	t.is(locale, expectedFallback, 'Locale did not match expected fallback');
+	t.not(locale, 'spawn_NOTALLOWED', 'Attempted to spawn subprocess');
 });
 
-test.serial('sync without spawn', t => {
+test('sync without spawn', t => {
 	const beforeTest = {};
 
 	// unset env vars and cache for restoration
 	unsetEnvVars(beforeTest);
 
 	// mock execa.sync
-	if (execa.sync) {
-		beforeTest.execaSync = execa.sync;
-		execa.sync = () => {
-			t.false('Attempted to spawn subprocess');
-		};
-	}
+	beforeTest.execaSync = execa.sync;
+	execa.sync = () => {
+		t.false('Attempted to spawn subprocess');
+	};
 
 	// test sync method
 	const locale = requireUncached('./').sync({spawn: false});
